a7648be0c80578a05c896a710144204edd650bef
Firdavsy07 <firdavsmuqimtoshov@gmail.com>
2023-02-08 10:16:27 -0500

    uhoh

---
+++ create-task/js/array.js
@@ -0,0 +1,17 @@
+const weapons = [
+  {
+    greatSword: { baseDamage: 140, toHit: 3, parry: "false" },
+  },
+  { pike: { baseDamage: 180, toHit: 4, parry: "false" } },
+  {
+    spearAndShield: { baseDamage: 100, toHit: 2, parry: "true" },
+  },
+  {
+    battleAxe: { baseDamage: 240, toHit: 5, parry: "false" },
+  },
+  {
+    quarterstaff: { baseDamage: 80, toHit: 1, parry: "true" },
+  },
+];
+
+const armor = [{}];
