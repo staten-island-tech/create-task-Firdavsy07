87dc49bba90822fec19e17b7586d58632c35cbd9
Firdavsy07 <firdavsmuqimtoshov@gmail.com>
2023-02-27 10:16:16 -0500

    card updates

--- create-task/js/main.js
+++ create-task/js/main.js
@@ -8,26 +8,56 @@ const DOMselectors = {
   area3: document.querySelector(".area3Container"),
   testButton: document.querySelector(".button"),
   deck: document.querySelector(".deck"),
+  selection: document.querySelector(".button"),
 };
-
+function destroy() {
+  const cards = document.querySelectorAll(".card");
+  cards.forEach((card) => {
+    card.remove();
+  });
+}
 DOMselectors.testButton.addEventListener("click", function () {
+  destroy();
   unitCards.forEach((unitCards, {}) => {
     DOMselectors.deck.insertAdjacentHTML(
       "afterbegin",
       `
       <div class="card">
       <div class="card-front">
+      
         <img
           class="card-img"
           src="/images/Cardback_Emblem_Sentinel_Icon_Crispmip.png "
-        />
+        /> 
       </div>
       <div class="card-back">
         <img class="card-img" src="${unitCards.uuid.cardFront}" />
+        <button class="button">Select me?</button>
       </div>
     </div>
 
 `
     );
   });
+});
+DOMselectors.selection.addEventListener("click", function () {
+  DOMselectors.card.insertAdjacentHTML(
+    "beforebegin",
+    `
+    <div class="card">
+    <div class="card-front">
+    
+      <img
+        class="card-img"
+        src="/images/Cardback_Emblem_Sentinel_Icon_Crispmip.png "
+      /> 
+    </div>
+    <div class="card-back">
+      <img class="card-img" src="${unitCards.uuid.cardFront}" />
+      <button class="button">Select me?</button>
+    </div>
+  </div>
+
+`
+  );
 });
