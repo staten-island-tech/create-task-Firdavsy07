d977c9101aec348f0e57498acd162ffa43d537b9
Firdavsy07 <firdavsmuqimtoshov@gmail.com>
2023-02-28 10:12:34 -0500

    commit
    fix button

--- create-task/js/main.js
+++ create-task/js/main.js
@@ -8,7 +8,6 @@ const DOMselectors = {
   area3: document.querySelector(".area3Container"),
   testButton: document.querySelector(".button"),
   deck: document.querySelector(".deck"),
-  selection: document.querySelector(".button"),
 };
 function destroy() {
   const cards = document.querySelectorAll(".card");
@@ -32,32 +31,15 @@ DOMselectors.testButton.addEventListener("click", function () {
       </div>
       <div class="card-back">
         <img class="card-img" src="${unitCards.uuid.cardFront}" />
-        <button class="button">Select me?</button>
+        <button id="bttn">Select Me?</button>
       </div>
     </div>
 
 `
     );
   });
-});
-DOMselectors.selection.addEventListener("click", function () {
-  DOMselectors.card.insertAdjacentHTML(
-    "beforebegin",
-    `
-    <div class="card">
-    <div class="card-front">
-    
-      <img
-        class="card-img"
-        src="/images/Cardback_Emblem_Sentinel_Icon_Crispmip.png "
-      /> 
-    </div>
-    <div class="card-back">
-      <img class="card-img" src="${unitCards.uuid.cardFront}" />
-      <button class="button">Select me?</button>
-    </div>
-  </div>
-
-`
-  );
+  const button = document.querySelectorAll("#bttn");
+  button.addEventListener("click", function () {
+    button.remove();
+  });
 });
