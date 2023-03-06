e9772ebc6d511457e534d3d2ee9defadfb432d57
Firdavsy07 <113189550+Firdavsy07@users.noreply.github.com>
2023-03-05 19:57:21 -0500

    i love js

--- create-task/js/main.js
+++ create-task/js/main.js
@@ -3,12 +3,14 @@ import { unitCards } from "./array";
 console.log(unitCards);
 
 const DOMselectors = {
-  area1: document.querySelector(".area1Container"),
-  area2: document.querySelector(".area2Container"),
-  area3: document.querySelector(".area3Container"),
+  area1: document.querySelector(".area1"),
+  area2: document.querySelector(".area2"),
+  area3: document.querySelector(".area3"),
   testButton: document.querySelector(".button"),
   deck: document.querySelector(".deck"),
+  Rerick: document.querySelector("#Rerick"),
 };
+const playedCards = [];
 function destroy() {
   const cards = document.querySelectorAll(".card");
   cards.forEach((card) => {
@@ -21,7 +23,7 @@ DOMselectors.testButton.addEventListener("click", function () {
     DOMselectors.deck.insertAdjacentHTML(
       "afterbegin",
       `
-      <div class="card">
+      <div class="card" id="c${unitCards.uuid.cardName}">
       <div class="card-front">
       
         <img
@@ -31,15 +33,96 @@ DOMselectors.testButton.addEventListener("click", function () {
       </div>
       <div class="card-back">
         <img class="card-img" src="${unitCards.uuid.cardFront}" />
-        <button id="bttn">Select Me?</button>
+        <button id="${unitCards.uuid.cardName}1">P1?</button>
+        <button id="${unitCards.uuid.cardName}2">P2?</button>
+        <button id="${unitCards.uuid.cardName}3">P3?</button>
       </div>
     </div>
 
 `
     );
   });
-  const button = document.querySelectorAll("#bttn");
-  button.addEventListener("click", function () {
-    button.remove();
+  const cardIds = {
+    cRerick: document.querySelector("#cRerick"),
+    Rerick1: document.querySelector("#Rerick1"),
+    Rerick2: document.querySelector("#Rerick2"),
+    Rerick3: document.querySelector("#Rerick3"),
+  };
+  console.log(cardIds);
+  //*Rerick*//
+  cardIds.Rerick1.addEventListener("click", function () {
+    cardIds.cRerick.remove();
+    unitCards
+      .filter((card) => card.uuid.cardName.includes("Rerick"))
+      .forEach((unitCards, {}) => {
+        DOMselectors.area1.insertAdjacentHTML(
+          "afterbegin",
+          `
+        <div class="card" id="d${unitCards.uuid.cardName}">
+        <div class="card-front">
+        
+          <img
+            class="card-img"
+            src="/images/Cardback_Emblem_Sentinel_Icon_Crispmip.png "
+          /> 
+        </div>
+        <div class="card-back">
+          <img class="card-img" src="${unitCards.uuid.cardFront}" />
+        </div>
+      </div>
+  
+  `
+        );
+      });
+  });
+  cardIds.Rerick2.addEventListener("click", function () {
+    cardIds.cRerick.remove();
+    unitCards
+      .filter((card) => card.uuid.cardName.includes("Rerick"))
+      .forEach((unitCards, {}) => {
+        DOMselectors.area2.insertAdjacentHTML(
+          "afterbegin",
+          `
+        <div class="card" id="d${unitCards.uuid.cardName}">
+        <div class="card-front">
+        
+          <img
+            class="card-img"
+            src="/images/Cardback_Emblem_Sentinel_Icon_Crispmip.png "
+          /> 
+        </div>
+        <div class="card-back">
+          <img class="card-img" src="${unitCards.uuid.cardFront}" />
+        </div>
+      </div>
+  
+  `
+        );
+      });
+  });
+  cardIds.Rerick3.addEventListener("click", function () {
+    cardIds.cRerick.remove();
+    unitCards
+      .filter((card) => card.uuid.cardName.includes("Rerick"))
+      .forEach((unitCards, {}) => {
+        DOMselectors.area3.insertAdjacentHTML(
+          "afterbegin",
+          `
+        <div class="card" id="d${unitCards.uuid.cardName}">
+        <div class="card-front">
+        
+          <img
+            class="card-img"
+            src="/images/Cardback_Emblem_Sentinel_Icon_Crispmip.png "
+          /> 
+        </div>
+        <div class="card-back">
+          <img class="card-img" src="${unitCards.uuid.cardFront}" />
+        </div>
+      </div>
+  
+  `
+        );
+      });
   });
 });
